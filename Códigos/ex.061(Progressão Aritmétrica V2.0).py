94d7148317f048bbd34fb0bbf74f9c08beb37e1b
Ander <andergabryel@gmail.com>
2024-09-13 14:19:07 -0300

    Create ex.061(Progressão Aritmétrica V2.0).py

---
+++ Códigos/ex.061(Progressão Aritmétrica V2.0).py
@@ -0,0 +1,10 @@
+##########################################
+print ("•×"*19)
+print ("\033[1;34m      Aula 13 - Repetição com FOR \033[m")
+print ("\033[0;32m   Desafio 060: Progressão Aritmétrica 2.0  \033[m")    
+print ("•×"*19)
+print("")
+##########################################
+'''Desafio: Programa exige primeiro termo e razão PA, após isso realiza a progressão'''
+
+#versão
