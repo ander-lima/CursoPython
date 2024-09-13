173a2d82c1013ba63d5506e836370a58c7f8512a
Ander <andergabryel@gmail.com>
2024-09-13 14:12:12 -0300

    Correção 060
    Versão do Professor Guanabara

--- Códigos/ex.060(Cálculo de Fatorial).py
+++ Códigos/ex.060(Cálculo de Fatorial).py
@@ -7,7 +7,7 @@ print("")
 ##########################################
 '''Desafio: Faça um programa que dado um número, calcule seu fatorial'''
 
-#versão Ander
+'''#versão Ander: Falhei
 #Fatorial é a soma da multiplicação de 1 número com 1 numero menor que o anterior multiplicado
 
 n = int(input('Digite um número para seu fatorial: '))
@@ -16,4 +16,18 @@ aux = n # Variável que usaremos para calcular o fatorial
 
 while prox > 1:
     fat *= aux # Multiplica fat pelo valor de aux
-    aux -= 1 # Diminui o valor de aux a cada passo
+    aux -= 1 # Diminui o valor de aux a cada passo'''
+
+#versão Guanabara
+
+n = int(input('Insira um número para seu fatorial: '))
+c = n #Determinando contador que começa no valor máximo que é igual ao número que se deseja fatorial
+f = 1 #O inicio para multiplicação/Caso nulo é = 1, assim como na soma o caso Nulo é = 0
+
+print('Calculando {}!= '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else  ' = ', end='')
+    f *= c
+    c -= 1
+print ('{}'.format(f))
