17ce68592e3a9db2135f01ada3bac432ee1abe38
Ander <andergabryel@gmail.com>
2024-09-19 17:14:04 -0300

    Update ex.063(Fibonacci v1.0).py

--- Códigos/ex.063(Fibonacci v1.0).py
+++ Códigos/ex.063(Fibonacci v1.0).py
@@ -9,7 +9,7 @@ print("")
 #Lógica Fibonacci: Primeiro termo é 0 e o segundo e terceiro são 1 > Prox = ultimo + penulmo
 #Lógica: Pergunta termo e armazena em variavel > enquanto contador for menor que termo > print fibo
 
-#versão Ander
+'''#versão Ander
 ciclos = int(input('Quantos termos você deseja mostrar ? '))
 count = 0
 fibo = 0
@@ -24,4 +24,22 @@ while ciclos > count:
     count +=1
 print ('FIM')
 print('')
-print('~~'*10)
+print('~~'*10)'''
+
+#versão Guanabara
+
+n = int(input('Quantos termos você quer ver ? '))
+t1 = 0
+t2 = 1
+
+print('~'*30)
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print (' -> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print (' -> FIM')
+print('~'*30)
