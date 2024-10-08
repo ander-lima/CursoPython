3c51713391fee2af7bc7f37b89431b97eb3f1c57
Ander <andergabryel@gmail.com>
2024-10-08 16:12:49 -0300

    ex. 068

--- Códigos/ex.068(Jogo impar ou par).py
+++ Códigos/ex.068(Jogo impar ou par).py
@@ -41,8 +41,22 @@ while True:
     computador = randint(0, 10)
     total = jogador + computador
     tipo = ' '
-    while tipo is not in 'PI':
-        tipo =  str(input('Par ou Ímpar ? [P/I]')).strip().uper()[0]
-        print(f'Você jogou {jogador} e o computador {computador}. Total de `{total}', end = '')
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar ? [P/I] ')).strip().upper()[0]
+        print(f'Você jogou {jogador} e o computador {computador}. Total de {total}', end = '')
         print ('Deu par' if total % 2 == 0 else 'Deu ímpar')
         if tipo == 'P':
+            if total % 2 == 0:
+                print('Você venceu')
+                v += 1
+            else:
+                print ('Você perdeu')
+                break
+        elif tipo == 'I':
+            if total % 2 != 0:
+                print ('Você venceu')
+                v += 1
+            else:
+                print('Você perdeu')
+                break
+        print ('vamos jogar novamente')
