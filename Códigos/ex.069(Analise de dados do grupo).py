f28537357ca7e22ff2ca354dd240babb01b9168a
Ander <andergabryel@gmail.com>
2024-10-14 15:04:58 -0300

    Inicio ex. 069

---
+++ Códigos/ex.069(Analise de dados do grupo).py
@@ -0,0 +1,10 @@
+##########################################
+print ("•×"*19)
+print ("\033[1;34m      Aula 15 - Break \033[m")
+print ("\033[0;32m   Desafio 069: Analise de dados do grupo  \033[m")    
+print ("•×"*19)
+print("")
+##########################################
+'''Desafio: '''
+
+#versão Ander
--- Códigos/teste.py
+++
@@ -1,8 +0,0 @@
-def recursiva(x):
-    if x == 1:
-        return -x
-    else:
-        return -5 * recursiva(x - 1) + x
-
-resultado = recursiva(4)
-print(resultado)
