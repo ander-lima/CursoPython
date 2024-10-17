b713af9e87b1224d8a2e0f127bfd39bd1e67840d
Ander <andergabryel@gmail.com>
2024-10-17 15:04:11 -0300

    Update ex.069(Analise de dados do grupo).py

--- Códigos/ex.069(Analise de dados do grupo).py
+++ Códigos/ex.069(Analise de dados do grupo).py
@@ -5,6 +5,23 @@ print ("\033[0;32m   Desafio 069: Analise de dados do grupo  \033[m")
 print ("•×"*19)
 print("")
 ##########################################
-'''Desafio: '''
+'''Desafio: Programa deve ler até indicado que não, perguntando idade e esexo de pessoas e depois puxar total de pessoas com mais de 18 anos, quantos homens e quantas mulheres com menos de 20 anos'''
 
-#versão Ander
+'''#versão Ander
+cont = 0
+while True:
+    loop = input('Quer continuar ? [S/N]').strip().lower()[0]
+    print('-'*20)
+    print ('Cadastre uma Pessoa')
+    print('-'*20)
+
+    idade = int(input('Idade:'))
+    if cont == 0:
+
+    sexo = str(input('Sexo:'))
+
+    if loop == 'n':
+        break
+print('ACABOU')'''
+
+#versão Guanabara
