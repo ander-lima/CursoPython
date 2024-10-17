32b56ce65767f2c28a7d9531ccd0d283632fefee
Ander <andergabryel@gmail.com>
2024-10-17 15:44:18 -0300

    Update ex.069(Analise de dados do grupo).py

--- Códigos/ex.069(Analise de dados do grupo).py
+++ Códigos/ex.069(Analise de dados do grupo).py
@@ -7,21 +7,60 @@ print("")
 ##########################################
 '''Desafio: Programa deve ler até indicado que não, perguntando idade e esexo de pessoas e depois puxar total de pessoas com mais de 18 anos, quantos homens e quantas mulheres com menos de 20 anos'''
 
-'''#versão Ander
-cont = 0
+#versão Ander
+''' pessoas_maiores = qnt_homens = qnt_mulheres = 0
 while True:
-    loop = input('Quer continuar ? [S/N]').strip().lower()[0]
     print('-'*20)
     print ('Cadastre uma Pessoa')
     print('-'*20)
 
     idade = int(input('Idade:'))
-    if cont == 0:
+    sexo = str(input('Sexo: [M/F]')).strip().lower()[0]
 
-    sexo = str(input('Sexo:'))
+    if idade >= 18:
+        pessoas_maiores += 1
+    if sexo == 'm':
+        qnt_homens +=1
+    if sexo == 'f' and idade <= 20:
+        qnt_mulheres += 1
 
+
+    loop = input('Quer continuar ? [S/N]').strip().lower()[0]
     if loop == 'n':
         break
-print('ACABOU')'''
+    elif loop == 's':
+        continue
+
+print (f'Você cadastrou {pessoas_maiores} pessoas com mais de 18 anos')
+print(f'Você cadastrou {qnt_homens} homens no total')
+print(f'Você cadastrou {qnt_mulheres} mulheres com menos de 20 anos')
+print('')
+print('======= FIM DO PROGRAMA =======')'''
 
 #versão Guanabara
+tot_18 = tot_h = tot_m20 = 0
+while True:
+
+    idade = int(input('Idade:'))
+    sexo = ' '
+    while sexo not in 'mf': 
+        sexo = str(input('Sexo: [M/F]')).strip().lower()[0]
+
+        if idade >= 18:
+            tot_18 += 1
+        if sexo == 'm':
+            tot_h +=1
+        if sexo == 'f' and idade <= 20:
+            tot_m20 += 1
+    resp = ' '
+
+    while resp not in 'sn':
+        resp = input('Quer continuar ? [S/N]').strip().lower()[0]
+    if resp == 'n':
+        break
+
+print (f'Você cadastrou {tot_18} pessoas com mais de 18 anos')
+print(f'Você cadastrou {tot_h} homens no total')
+print(f'Você cadastrou {tot_m20} mulheres com menos de 20 anos')
+print('')
+print('======= FIM DO PROGRAMA =======')
