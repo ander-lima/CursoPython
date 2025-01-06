2c29de3c8d3b1ae6d37ce7ead75e1cf41c4f7a16
Ander <andergabryel@gmail.com>
2025-01-06 18:02:15 -0300

    Update ex.068(Jogo impar ou par).py
    Corrigindo erro ao perder e não encerrar jogo.

--- Códigos/ex.068(Jogo impar ou par).py
+++ Códigos/ex.068(Jogo impar ou par).py
@@ -43,20 +43,20 @@ while True:
     tipo = ' '
     while tipo not in 'PI':
         tipo = str(input('Par ou Ímpar ? [P/I] ')).strip().upper()[0]
-        print(f'Você jogou {jogador} e o computador {computador}. Total de {total}', end = '')
-        print ('Deu par' if total % 2 == 0 else 'Deu ímpar')
-        if tipo == 'P':
-            if total % 2 == 0:
-                print('Você venceu')
-                v += 1
-            else:
-                print ('Você perdeu')
-                break
-        elif tipo == 'I':
-            if total % 2 != 0:
-                print ('Você venceu')
-                v += 1
-            else:
-                print('Você perdeu')
-                break
-        print ('vamos jogar novamente')
+    print(f'Você jogou {jogador} e o computador {computador}. O total deu {total} ', end = '')
+    print ('Deu par' if total % 2 == 0 else 'Deu ímpar')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu')
+            v += 1
+        else:
+            print ('Você perdeu')
+            break
+    elif tipo == 'I':
+        if total % 2 != 0:
+            print ('Você venceu')
+            v += 1
+        else:
+            print('Você perdeu')
+            break
+    print ('Vamos jogar novamente')
